6a1b80a6dfae1b39d3372bee01714f767a007076
Timothy Loftus <tloftus@protonmail.com>
2024-10-07 23:07:58 -0500

    Init commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,9 @@
+vim.cmd('colorscheme poimandres')
+
+require('poimandres').setup({
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = false,
+})
---
+++ lua/n3s0/init.lua
@@ -0,0 +1,2 @@
+require("n3s0.set")
+require("n3s0.remap")
