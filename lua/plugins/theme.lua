5ebfbed6503680f54c48b892fe02bdfb5162a06a
Timothy Loftus <tloftus@protonmail.com>
2026-04-10 18:12:48 -0500

    update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,45 +1,5 @@
 -- ~/nvim/lua/n3s0/plugins/theme.lua
 
-<<<<<<< HEAD
-<<<<<<< HEAD:lua/n3s0/plugins/colorscheme.lua
---[====[ cyberdream colorschme
-return {
-  {
-    "scottmckendry/cyberdream.nvim",
-    dev = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("cyberdream").setup({
-        transparent = true,
-        italic_comments = true,
-        hide_fillchars = true,
-        terminal_colors = false,
-        cache = true,
-        borderless_telescope = { border = false, style = "flat" },
-        theme = { variant = "auto" },
-      })
-
-      vim.cmd("colorscheme cyberdream")
-      vim.api.nvim_set_keymap("n", "<leader>tt", ":CyberdreamToggleMode<CR>", { noremap = true, silent = true })
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "CyberdreamToggleMode",
-        callback = function(ev)
-          print("Switched to " .. ev.data .. " mode!")
-        end,
-      })
-    end,
-  },
-}
-]====]---
-
---[ rose-pine colorschme
-=======
--- rose-pine colorschme
->>>>>>> 46d8296 (Restructure):lua/plugins/theme.lua
-=======
--- rose-pine colorschme
->>>>>>> 46d8296 (Restructure)
 return {
   "rose-pine/neovim",
   name = "rose-pine",
